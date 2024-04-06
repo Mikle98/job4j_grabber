42dbe0f1c292d5e9c542fc2cc8b36017da304235
mikle <mikle>
2024-04-06 23:18:13 +0300

    2.1. Преобразование даты [#289476 #490132]

--- src/main/java/ru/job4j/quartz/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/quartz/grabber/utils/HabrCareerDateTimeParser.java
@@ -3,11 +3,13 @@ package ru.job4j.quartz.grabber.utils;
 import java.time.LocalDateTime;
 
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        return LocalDateTime.parse(parse.split("\\+")[0], ISO_LOCAL_DATE_TIME);
+        LocalDateTime time = LocalDateTime.parse(parse, ISO_OFFSET_DATE_TIME);
+        return LocalDateTime.parse(time.format(ISO_LOCAL_DATE_TIME));
     }
 }
