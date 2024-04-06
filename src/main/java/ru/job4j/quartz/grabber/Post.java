48f4b1f5035cbde38ba86c6b977102b07d01ef3f
mikle <mikle>
2024-04-06 13:33:02 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/quartz/grabber/Post.java
@@ -0,0 +1,41 @@
+package ru.job4j.quartz.grabber;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    public int id;
+    public String title;
+    public String link;
+    public String description;
+    public LocalDateTime created;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created
+                + '}';
+    }
+}
